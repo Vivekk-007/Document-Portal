a9d43a31f38e558270977d48a15ea7cdc87188a3
vivekkk-007 <kumarvivek05093896@gmail.com>
2026-08-01 23:15:40 +0530

    docker ready

--- utils/config_loader.py
+++ utils/config_loader.py
@@ -1,6 +1,9 @@
+from pathlib import Path
 import yaml
 
-def load_config(config_path:str ="config\config.yaml") -> dict:
-    with open(config_path, 'r') as file:
-        config = yaml.safe_load(file)
-    return config
+BASE_DIR = Path(__file__).resolve().parent.parent
+CONFIG_PATH = BASE_DIR / "config" / "config.yaml"
+
+def load_config():
+    with open(CONFIG_PATH, "r", encoding="utf-8") as file:
+        return yaml.safe_load(file)
